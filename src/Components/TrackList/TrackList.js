1e5f1ee2ccae0aa696d31f923cc524aa74a765a9
Carlos Ramirez <carlosramirez@carloss-mbp.home>
2020-10-29 20:32:52 -0400

    Added Track component and fixed import statement in TrackList

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './TrackList.css';
-// import Track from '';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-          {/* <Track /> */}
+          <Track />
       </div>
     );
   }
