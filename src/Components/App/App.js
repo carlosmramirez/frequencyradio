d5cbd751cdb868f281e18b9062baa1d334822134
Carlos Ramirez <carlosramirez@carloss-mbp.home>
2020-10-30 23:45:58 -0400

    Added search results state and passed to other comps

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,31 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'song1',
+          artist: 'artist1',
+          album: 'album1',
+          id: 1
+        },
+        {
+          name: 'song2',
+          artist: 'artist2',
+          album: 'album2',
+          id: 2
+        },
+        {
+          name: 'song3',
+          artist: 'artist3',
+          album: 'album3',
+          id: 3
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div>
@@ -12,7 +37,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults /> 
+            <SearchResults searchResults={this.state.searchResults} /> 
             <Playlist /> 
           </div>
         </div>
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import TrackList from '';
+import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
 
 class SearchResults extends React.Component {
+
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        {/* <TrackList /> */}
+        <TrackList tracks={this.props.searchResults} />
       </div>
     );
   }
