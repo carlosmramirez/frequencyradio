2e135bdbd78b9369da1c5627319fcef2eec77f7c
Carlos Ramirez <carlosramirez@carloss-mbp.home>
2020-11-03 17:44:24 -0500

    Update search results with user search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -54,6 +54,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -80,13 +81,17 @@ class App extends React.Component {
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
   }
+
+  search(term) {
+    console.log(term);
+  }
  
   render() {
     return (
       <div>
         <h1>The <span className="highlight">Frequen</span>cy</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults 
               searchResults={this.state.searchResults} 
